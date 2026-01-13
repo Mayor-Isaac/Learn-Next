e1c03ab6770fd30de0f4238250f8ca31702ef6bb
Mayor-Isaac <isaacogunyileka@gmail.com>
2026-01-13 17:25:41 +0100

    feat: integrate PostHog for analytics tracking
    - Added posthog-js as a dependency in package.json.
    - Updated pnpm-lock.yaml to include posthog-js and its dependencies.
    - Created PostHogProvider component to initialize PostHog and wrap the application.
    - Added instrumentation-client.ts for PostHog initialization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/Components/LightRays";
 import Navbar from "@/Components/Navbar";
+import { PostHogProvider } from "./providers";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -29,21 +30,23 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased relative`}
       >
-        <Navbar />
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen ">
-          <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={0.5}
-            lightSpread={0.9}
-            rayLength={1.4}
-            followMouse={true}
-            mouseInfluence={0.02}
-            noiseAmount={0}
-            distortion={0.01}
-          />
-        </div>
-        {children}
+        <PostHogProvider>
+          <Navbar />
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen ">
+            <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.02}
+              noiseAmount={0}
+              distortion={0.01}
+            />
+          </div>
+          {children}
+        </PostHogProvider>
       </body>
     </html>
   );
